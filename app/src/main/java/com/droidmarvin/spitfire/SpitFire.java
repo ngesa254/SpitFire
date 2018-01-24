9fcd20ce901b67395191b38af7c51952429c2b55
ngesa marvin <marvinngesa@gmail.com>
2018-01-24 23:23:01 +0300

    enable calligraphy

--- app/src/main/java/com/droidmarvin/spitfire/SpitFire.java
+++ app/src/main/java/com/droidmarvin/spitfire/SpitFire.java
@@ -3,11 +3,20 @@ package com.droidmarvin.spitfire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 public class SpitFire extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setContentView(R.layout.activity_spit_fire);
     }
 }
