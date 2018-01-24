56375b314538ae3e1fbbc06f8f373c1c7f0bd48d
ngesa marvin <marvinngesa@gmail.com>
2018-01-24 23:29:16 +0300

    grab the buttons form the layout

--- app/src/main/java/com/droidmarvin/spitfire/SpitFire.java
+++ app/src/main/java/com/droidmarvin/spitfire/SpitFire.java
@@ -2,10 +2,13 @@ package com.droidmarvin.spitfire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class SpitFire extends AppCompatActivity {
+
+    private Button mSpit, mFire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,9 @@ public class SpitFire extends AppCompatActivity {
                 .build());
 
         setContentView(R.layout.activity_spit_fire);
+
+        mSpit = (Button) findViewById(R.id.btnSpit);
+        mFire = (Button) findViewById(R.id.btnFire);
+
     }
 }
