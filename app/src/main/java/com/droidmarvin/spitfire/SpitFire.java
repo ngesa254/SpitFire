35d226abdfb76e6df61a04f4025d7581279549e9
ngesa marvin <marvinngesa@gmail.com>
2018-01-24 23:03:42 +0300

    create app, add layout and dependencies

---
+++ app/src/main/java/com/droidmarvin/spitfire/SpitFire.java
@@ -0,0 +1,13 @@
+package com.droidmarvin.spitfire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpitFire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spit_fire);
+    }
+}
