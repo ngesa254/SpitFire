0648e6e234935a0c11528f1f2463fd79b2f14ea6
ngesa marvin <marvinngesa@gmail.com>
2018-01-24 23:35:17 +0300

    set up firebase to spit fire

--- app/src/main/java/com/droidmarvin/spitfire/SpitFire.java
+++ app/src/main/java/com/droidmarvin/spitfire/SpitFire.java
@@ -4,11 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class SpitFire extends AppCompatActivity {
 
     private Button mSpit, mFire;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mSpace;
+
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,10 @@ public class SpitFire extends AppCompatActivity {
 
         mSpit = (Button) findViewById(R.id.btnSpit);
         mFire = (Button) findViewById(R.id.btnFire);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        userId = mAuth.getCurrentUser().getUid();
 
     }
 }
